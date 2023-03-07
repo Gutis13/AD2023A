55eb12f7b8f2bc44b173c69c77bc19dc5a813bcd
Erick Gutiérrez <87037026+Gutis13@users.noreply.github.com>
2023-03-06 21:55:35 -0600

    Add files via upload

---
+++ Actividad15/Actividad 15.R
@@ -0,0 +1,51 @@
+#Regresion Lineal Simple 
+#Erick Gutiérrez Rubio
+#Código: 219284107
+#Actividad 15 
+#Importar el dataset 
+dataset= read.csv('Salary_Data.csv') 
+#dataset = dataset [, 2: 3] 
+
+#Dividir los datos en conjunto de entrenamiento y conjunto de test 
+#install.packages("caTools") 
+library(caTools)
+set.seed(123) 
+split= sample.split(dataset$Salary, SplitRatio= 2/3)
+training_set = subset(dataset, split == TRUE)
+testing_set = subset(dataset, split == FALSE)
+
+#Ajustar el modelo de regresion lineal simple con el conjunto de entrenamiento
+regressor= lm(formula = Salary ~ YearsExperience,
+             data = training_set)
+summary (regressor) 
+# Descomentar para revisar y analizar la regresión
+
+#Predecir resultados con el conjunto de test 
+y_pred = predict(regressor, newdata = testing_set) 
+
+#Visualizacion de los resultados en el conjunto de entrenamiento 
+#install.packages("ggplot2")
+library(ggplot2)
+ggplot () + 
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = "red") + 
+  geom_line(aes(x = training_set$YearsExperience, y= predict (regressor, newdata = training_set)), 
+            colour = "blue") + 
+  ggtitle("Sueldo vs Años de Experiencia (Conjunto de Entrenamiento)")+ 
+  xlab("Años de Experiencia
+       ERICK GUTIÉRREZ RUBIO")+
+  ylab("Sueldo (en $)")
+
+#Visualizacion de los resultados en el conjunto de testing
+  ggplot () + 
+  geom_point(aes(x = testing_set$YearsExperience, y = testing_set$Salary),
+             colour = "red") + 
+  geom_line(aes(x = training_set$YearsExperience, y= predict (regressor, newdata = training_set)), 
+            colour = "blue") + 
+  ggtitle("Sueldo vs Años de Experiencia (Conjunto de Testing)")+ 
+  xlab("Años de Experiencia      
+    ERICK GUTIÉRREZ RUBIO")+
+   ylab("Sueldo (en $)")
+
+
+
