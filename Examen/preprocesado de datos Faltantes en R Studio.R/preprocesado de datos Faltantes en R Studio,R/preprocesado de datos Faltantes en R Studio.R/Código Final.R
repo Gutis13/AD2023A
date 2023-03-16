9e631151121f531f4d5257f3d10a72edb6c8e5df
Erick Gutiérrez <87037026+Gutis13@users.noreply.github.com>
2023-03-16 01:57:26 -0600

    Update Código Final.R

--- Examen/preprocesado de datos Faltantes en R Studio.R/preprocesado de datos Faltantes en R Studio,R/preprocesado de datos Faltantes en R Studio.R/Código Final.R
+++ Examen/preprocesado de datos Faltantes en R Studio.R/preprocesado de datos Faltantes en R Studio,R/preprocesado de datos Faltantes en R Studio.R/Código Final.R
@@ -1,34 +1,24 @@
 dataset = read.csv('D:/9 Semestre/EXAMEN/Data.csv')
 
 dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, 
-                         FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age) # return column
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
 
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                        dataset$Salary)
+dataset$Salary= ifelse(is.na(dataset$Salary), 
+                       ave(dataset$Salary, FUN = function(x) mean (x, na.rm= TRUE)),
+                      dataset$Salary)  
 
-dataset = read.csv('D:/9 Semestre/EXAMEN/Data.csv')
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('No', 'Yes'),
-                           labels = c(0, 1))
 
 dataset = read.csv('D:/9 Semestre/EXAMEN/Data.csv')
-library(caTools)
+dataset$Country = factor (dataset$Country, levels = c("France", "Spain", "Germany"), 
+                          labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased, levels = c("No", "Yes"), 
+                           labels = c(0,1)) 
 
-set.seed(123)
-
-#------ Train / Test split
-#                    dataset$DependentVariable
-split = sample.split(dataset$Purchased, 
-                     SplitRatio = 0.8) # returns T/F, 
-
-# True => Training set, False => Test set
+dataset = read.csv('D:/9 Semestre/EXAMEN/Data.csv')
+library(caTools) 
+set.seed(123) 
+split = sample.split(dataset$Purchased, SplitRatio =0.8)
 training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+testing_set = subset(dataset, split == FALSE) 
 
